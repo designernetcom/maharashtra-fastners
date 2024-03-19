865f338ec6216af4616fe9bfc3aa06e20aec1242
Dhyey Vithalani <dhyey97@gmail.com>
2024-03-19 12:19:10 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -990,6 +990,37 @@
     });
 
 
+    
+$(document).ready(function(){
+	progress_bar();
+});
+
+function progress_bar() {
+	var speed = 30;
+	var items = $('.progress_bar').find('.progress_bar_item');
+	
+    items.each(function() {
+        var item = $(this).find('.progress');
+        var itemValue = item.data('progress');
+        var i = 0;
+        var value = $(this);
+		
+        var count = setInterval(function(){
+            if(i <= itemValue) {
+                var iStr = i.toString();
+                item.css({
+                    'width': iStr+'%'
+                });
+                value.find('.item_value').html(iStr +'%');
+            }
+            else {
+                clearInterval(count);
+            }
+            i++;
+        },speed);
+    });
+}
+
 
     /*==========================================================================
         WHEN WINDOW SCROLL
